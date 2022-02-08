80dc65cb91e271ee85dfe2dbf0e746a5238a7a7f
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2022-02-08 09:23:52 +0100

    Mise en page des Technologies sur Accueil

--- src/components/Accueil/Accueil.jsx
+++ src/components/Accueil/Accueil.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState, useEffect } from 'react';
 import './Accueil.css';
 import Modal from '../Modal/Modal';
 import grandePhoto from '../../assets/jpg/grande-photo.jpg';
 import petitePhoto from '../../assets/png/petite-photo-ronde.png';
+import html5 from '../../assets/svg/html5.svg';
+import css3 from '../../assets/svg/css3.svg';
+import javascript from '../../assets/svg/javascript.svg';
+import nodejs from '../../assets/svg/nodejs.svg';
+import react from '../../assets/svg/react.svg';
+import mysql from '../../assets/svg/mysql.svg';
+import git from '../../assets/svg/git.svg';
+import github from '../../assets/svg/github-original.svg';
+import certificatHtml5Css3 from '../../assets/pdf/CertificatDeReussite-ApprenezACréerVotreSiteWebAvecHtml5EtCss3.pdf';
 
 function Accueil({ setTitle }) {
   const [openModal, setOpenModal] = useState(false);
+  const [openModal2, setOpenModal2] = useState(false);
   useEffect(() => {
     setTitle('Accueil');
   }, []);
@@ -29,6 +41,62 @@ function Accueil({ setTitle }) {
           ici, vous trouverez mes créations, mes chefs-d&#39;oeuvres et plus
           encore...
         </p>
+      </div>
+      <div className="techno">
+        <ul className="logo">
+          <li>
+            <a
+              className="certificat"
+              href={certificatHtml5Css3}
+              onClick={() => setOpenModal2(true)}
+            >
+              HTML5
+              <img
+                src={html5}
+                alt="Logo Html5"
+                onClick={() => setOpenModal2(true)}
+              />
+            </a>
+            {openModal2 && (
+              <Modal closeModal={setOpenModal2} img={certificatHtml5Css3} />
+            )}
+          </li>
+          <li>
+            <a
+              className="certificat"
+              href={certificatHtml5Css3}
+              target="_blank"
+              rel="noreferrer"
+            >
+              CSS3
+              <img src={css3} alt="Logo Css3" />
+            </a>
+          </li>
+          <li>
+            javascript
+            <img src={javascript} alt="Logo Javascript" />
+          </li>
+          <li>
+            Node JS
+            <img src={nodejs} alt="Logo Node JS" />
+          </li>
+          <li>
+            React JS
+            <img src={react} alt="Logo React JS" />
+          </li>
+          <li>
+            Mysql
+            <img src={mysql} alt="Logo Mysql" />
+          </li>
+          <li>
+            Git
+            <img src={git} alt="Logo Git" />
+          </li>
+          <li>
+            Github
+            <img src={github} alt="Logo Github" />
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,19 +3,17 @@ import './Modal.css';
 
 function Modal({ closeModal, img }) {
   return (
-    <div className="modal-background">
-      <div className="modal-container">
-        <div className="title-close-btn">
-          <button
-            type="button"
-            className="x-btn"
-            onClick={() => closeModal(false)}
-          >
-            X
-          </button>
-        </div>
-        <img src={img} alt="" />
+    <div className="modal-container">
+      <div className="title-close-btn">
+        <button
+          type="button"
+          className="x-btn"
+          onClick={() => closeModal(false)}
+        >
+          X
+        </button>
       </div>
+      <img src={img} alt="" />
     </div>
   );
 }
